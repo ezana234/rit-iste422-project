e9727e317c7fe24521561cef41b5e963a3dfa800
repl.it user <replituser@example.com>
2020-09-16 17:34:21 +0000

    Fixed errors in build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 echo "Cleaning existing classes..."
-rm -f build/src/main/java/*.class
+rm -f build/src/*/java/*.class
 # This command looks for matching files and runs the rm command for each file
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/*/java/*.java
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/main/java/*.java src/test/java/*.java -d ./build
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore src.test.java.EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:./build org.junit.runner.JUnitCore src.test.java.EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java src/main/java/RunEdgeConvert
+java -cp ./build src/main/java/RunEdgeConvert
